1d0e4b646095c2f421fd6543fd40398ce9cd841a
Akshay Khajuriya <241728994+cx-akshay-khajuriya@users.noreply.github.com>
2026-09-18 12:35:54 +0530

    Update getUser.java

--- getUser.java
+++ getUser.java
@@ -6,7 +6,7 @@ public class UserDao {
     public ResultSet getUserById(HttpServletRequest req, Connection conn) throws SQLException {
         String id = req.getParameter("id");
         Statement stmt = conn.createStatement();
-        // SQL Injection - dd concazxczxczt!
+        // SQL Injection - dd concazxasdsadczxczt!
         return stmt.executeQuery("SELECT * FROM users WHERE id = " + id);
     }
 
